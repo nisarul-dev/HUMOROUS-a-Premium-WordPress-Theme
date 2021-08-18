e84194271eb8ecd114859057a83e73aadc8daa82
nisarul-media <65971192+nisarul-media@users.noreply.github.com>
2021-08-19 00:18:11 +0600

    Created Admin SubPages and Part-3 ends.

--- wp-content/themes/humorous-theme/inc/function-admin.php
+++ wp-content/themes/humorous-theme/inc/function-admin.php
@@ -7,21 +7,45 @@
  */
 
 function humorous_add_admin_page() {
+    // Generate Humorous Admin Page
 	add_menu_page(
 		'Humorous Theme Options', // page title
 		'Humorous', // menu title
 		'manage_options', // capability
 		'humorous_theme_options', // menu slug
-		'humorous_add_create_page', // function
+		'humorous_theme_admin_page', // function
 		get_template_directory_uri() . '/img/humorous-icon-white.svg', //icon url
 		100 // position
 	);
+
+	// Generate Humorous Admin Sub-Pages
+    add_submenu_page(
+        'humorous_theme_options',  // parent slug
+        'Humorous Theme Options', // page title
+        'General', // sub-menu title
+        'manage_options', // capability
+        'humorous_theme_options', // menu slug
+        'humorous_theme_admin_page' // function
+    );
+    add_submenu_page(
+        'humorous_theme_options', // parent slug
+        'Humorous CSS options', // page title
+        'Custom CSS', // sub-menu title
+        'manage_options', // capability
+        'humorous_custom_css', // menu slug
+        'humorous_theme_custom_css_page' // function
+    );
 }
 add_action('admin_menu', 'humorous_add_admin_page');
 
-function humorous_add_create_page() {
-	// Generation of admin page
+function humorous_theme_admin_page() {
+	// Generation of admin page (General)
+	require_once get_template_directory() . '/inc/templates/humorous-admin.php';
+}
+
+function humorous_theme_custom_css_page() {
+	// Generation of Custom CSS page
 	?>
-	<h1>Humorous Theme Options</h1>
-<?php
+    <h1>Custom CSS</h1>
+	<?php
 }
---
+++ wp-content/themes/humorous-theme/inc/templates/humorous-admin.php
@@ -0,0 +1,3 @@
+<h1>Humorous Theme Options</h1>
+<h3 class="title">Manage Options</h3>
+<p>Customize the default WordPress Appearance Options</p>
